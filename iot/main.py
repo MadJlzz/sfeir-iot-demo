137b70e1c9ed2035a522fda21483c8c1125b5a76
Julien Klaer <klaer.julien@gmail.com>
2018-10-28 01:00:39 +0200

    feat(sensor): Adding implementation of the DHT11 sensors.

--- iot/main.py
+++ iot/main.py
@@ -6,23 +6,11 @@ import json
 import jwt
 import paho.mqtt.client as mqtt
 
-# from iot.sensor import DHT11
+from iot.sensor import DHT11
 
 
 def create_jwt(project_id, private_key_file, algorithm):
-    """Creates a JWT (https://jwt.io) to establish an MQTT connection.
-        Args:
-         project_id: The cloud project ID this device belongs to
-         private_key_file: A path to a file containing either an RSA256 or
-                 ES256 private key.
-         algorithm: The encryption algorithm to use. Either 'RS256' or 'ES256'
-        Returns:
-            An MQTT generated from the given project_id and private key, which
-            expires in 20 minutes. After 20 minutes, your client will be
-            disconnected, and a new JWT will have to be generated.
-        Raises:
-            ValueError: If the private_key_file does not contain a known key.
-        """
+    """Creates a JWT (https://jwt.io) to establish an MQTT connection."""
 
     token = {
         # The time that the token was issued at
@@ -44,37 +32,25 @@ def create_jwt(project_id, private_key_file, algorithm):
 
 
 def error_str(rc):
-    """Convert a Paho error to a human readable string."""
     return '{}: {}'.format(rc, mqtt.error_string(rc))
 
 
 def on_connect(unused_client, unused_userdata, unused_flags, rc):
-    """Callback for when a device connects."""
-    print('on_connect', mqtt.connack_string(rc))
+    print('Trying to connect to the MQTT broker: ', mqtt.connack_string(rc))
 
 
 def on_disconnect(unused_client, unused_userdata, rc):
-    """Paho callback for when a device disconnects."""
-    print('on_disconnect', error_str(rc))
+    print('Disconnection from the MQTT broker. Reason: ', error_str(rc))
 
 
 def on_publish(unused_client, unused_userdata, unused_mid):
-    """Paho callback when a message is sent to the broker."""
-    print('on_publish')
-
-
-def on_message(unused_client, unused_userdata, message):
-    """Callback when the device receives a message on a subscription."""
-    payload = str(message.payload)
-    print('Received message \'{}\' on topic \'{}\' with Qos {}'.format(
-        payload, message.topic, str(message.qos)))
+    print('Message has been successfully published !')
 
 
 def get_client(
         project_id, cloud_region, registry_id, device_id, private_key_file,
         algorithm, ca_certs, mqtt_bridge_hostname, mqtt_bridge_port):
-    """Create our MQTT client. The client_id is a unique string that identifies
-    this device. For Google Cloud IoT Core, it must be in the format below."""
+
     client = mqtt.Client(
         client_id=('projects/{}/locations/{}/registries/{}/devices/{}'
                    .format(project_id, cloud_region, registry_id, device_id)))
@@ -89,13 +65,9 @@ def get_client(
     # Enable SSL/TLS support.
     client.tls_set(ca_certs=ca_certs, tls_version=ssl.PROTOCOL_TLSv1_2)
 
-    # Register message callbacks. https://eclipse.org/paho/clients/python/docs/
-    # describes additional callbacks that Paho supports. In this example, the
-    # callbacks just print to standard out.
     client.on_connect = on_connect
     client.on_publish = on_publish
     client.on_disconnect = on_disconnect
-    client.on_message = on_message
 
     # Connect to the Google MQTT bridge.
     client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
@@ -104,7 +76,7 @@ def get_client(
 
 
 def main():
-    # dht11 = DHT11()
+    dht11 = DHT11()
 
     project_id = "sfeir-iot-demo"
     cloud_region = "europe-west1"
@@ -123,23 +95,21 @@ def main():
         private_key_file, algorithm, ca_certs,
         mqtt_bridge_hostname, mqtt_bridge_port)
 
-    # Publish num_messages mesages to the MQTT bridge once per second.
+    # Publish DHT11 temperature and humidity values.
     while True:
         # Process network events.
         client.loop()
 
         # Get temperature and humidity
-        # dht11.read_dht11()
+        dht11.read_dht11()
 
-        # payload = {"temperature": dht11.temperature, "humidity": dht11.humidity}
-        payload = {"temperature": 24, "humidity": 30}
-        # payload = '{}/{}-payload'.format(registry_id, device_id)
+        payload = {"temperature": dht11.temperature, "humidity": dht11.humidity}
         print('Publishing message {}'.format(payload))
 
         client.publish(mqtt_topic, json.dumps(payload), qos=1)
 
-        # Send events every second. State should not be updated as often
-        time.sleep(1)
+        # Send events every three second.
+        time.sleep(3)
 
 
 if __name__ == '__main__':
