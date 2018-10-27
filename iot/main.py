a8aa82f3d316cbcfbac78fe1b4c93955780d00b8
Julien Klaer <klaer.julien@gmail.com>
2018-10-28 00:25:41 +0200

    fix(iot): Trying something else because some weird problems with last version.

--- iot/main.py
+++ iot/main.py
@@ -1,8 +1,9 @@
 import datetime
 import ssl
+import time
 
 import jwt
-import paho.mqtt.client as mqtt
+from paho import mqtt
 
 
 def create_jwt(project_id, private_key_file, algorithm):
@@ -39,36 +40,101 @@ def create_jwt(project_id, private_key_file, algorithm):
     return jwt.encode(token, private_key, algorithm=algorithm)
 
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
+def error_str(rc):
+    """Convert a Paho error to a human readable string."""
+    return '{}: {}'.format(rc, mqtt.error_string(rc))
+
+
+def on_connect(unused_client, unused_userdata, unused_flags, rc):
     """Callback for when a device connects."""
-    print("on_connect: " + mqtt.connack_string(rc))
+    print('on_connect', mqtt.connack_string(rc))
 
 
-iot_projectId = "sfeir-iot-demo"
-iot_cloudRegion = "europe-west1"
-iot_registryId = "sfeir-iot-registry"
-iot_deviceId = "pi-madpi"
+def on_disconnect(unused_client, unused_userdata, rc):
+    """Paho callback for when a device disconnects."""
+    print('on_disconnect', error_str(rc))
 
-public_certificate_path = "/home/pi/Documents/repositories/sfeir-iot-demo/certs/rsa_cert.pem"
-private_certificate_path = "/home/pi/Documents/repositories/sfeir-iot-demo/certs/rsa_private.pem"
 
-client = mqtt.Client("projects/{}/locations/{}/registries/{}/devices/{}".format(iot_projectId, iot_cloudRegion,
-                                                                                iot_registryId, iot_deviceId))
+def on_publish(unused_client, unused_userdata, unused_mid):
+    """Paho callback when a message is sent to the broker."""
+    print('on_publish')
 
-client.username_pw_set(
-    username='unused',
-    password=create_jwt(iot_projectId, private_certificate_path, "RS256")
-)
 
-client.tls_set(None, public_certificate_path, private_certificate_path, ssl.CERT_REQUIRED, ssl.PROTOCOL_TLS)
+def on_message(unused_client, unused_userdata, message):
+    """Callback when the device receives a message on a subscription."""
+    payload = str(message.payload)
+    print('Received message \'{}\' on topic \'{}\' with Qos {}'.format(
+        payload, message.topic, str(message.qos)))
 
-client.on_connect = on_connect
 
-client.connect("mqtt.googleapis.com", 8883, 60)
+def get_client(
+        project_id, cloud_region, registry_id, device_id, private_key_file,
+        algorithm, ca_certs, mqtt_bridge_hostname, mqtt_bridge_port):
+    """Create our MQTT client. The client_id is a unique string that identifies
+    this device. For Google Cloud IoT Core, it must be in the format below."""
+    client = mqtt.Client(
+        client_id=('projects/{}/locations/{}/registries/{}/devices/{}'
+            .format(
+            project_id,
+            cloud_region,
+            registry_id,
+            device_id)))
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+    # With Google Cloud IoT Core, the username field is ignored, and the
+    # password field is used to transmit a JWT to authorize the device.
+    client.username_pw_set(
+        username='unused',
+        password=create_jwt(
+            project_id, private_key_file, algorithm))
+
+    # Enable SSL/TLS support.
+    client.tls_set(ca_certs=ca_certs, tls_version=ssl.PROTOCOL_TLSv1_2)
+
+    # Register message callbacks. https://eclipse.org/paho/clients/python/docs/
+    # describes additional callbacks that Paho supports. In this example, the
+    # callbacks just print to standard out.
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+
+    # Connect to the Google MQTT bridge.
+    client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
+
+    return client
+
+
+def main():
+    mqtt_topic = '/devices/{}/{}'.format("pi-madpi", "events")
+
+    project_id = "sfeir-iot-demo"
+    cloud_region = "europe-west1"
+    registry_id = "sfeir-iot-registry"
+    device_id = "pi-madpi"
+    private_key_file = "certs/rsa_private.pem"
+    algorithm = "RS256"
+    ca_certs = "certs/roots.pem"
+    mqtt_bridge_hostname = "mqtt.googleapis.com"
+    mqtt_bridge_port = 8883
+
+    client = get_client(
+        project_id, cloud_region, registry_id, device_id,
+        private_key_file, algorithm, ca_certs,
+        mqtt_bridge_hostname, mqtt_bridge_port)
+
+    # Publish num_messages mesages to the MQTT bridge once per second.
+    while True:
+        # Process network events.
+        client.loop()
+
+        payload = '{}/{}-payload'.format(registry_id, device_id)
+        print('Publishing message {}'.format(payload))
+
+        client.publish(mqtt_topic, payload, qos=1)
+
+        # Send events every second. State should not be updated as often
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
